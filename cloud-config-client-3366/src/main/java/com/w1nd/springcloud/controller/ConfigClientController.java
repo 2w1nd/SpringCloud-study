afec3f0299b3edf08030172c2e975aa181cdbd41
w1nd <584202045@qq.com>
2021-09-26 18:44:02 +0800

    p82

---
+++ cloud-config-client-3366/src/main/java/com/w1nd/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.w1nd.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @author: w1nd
+ * @date: 2021年09月26日 18:20
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort:" + serverPort + "\t\n\n configInfo: " + configInfo;
+    }
+
+}
