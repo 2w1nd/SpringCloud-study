d2a58af40f458313c8977ce3cddbabf920106330
w1nd <584202045@qq.com>
2021-09-27 16:22:15 +0800

    P94

--- cloud-provider-payment8001/src/main/java/com/w1nd/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/w1nd/springcloud/controller/PaymentController.java
@@ -82,4 +82,14 @@ public class PaymentController {
         return serverPort;
     }
 
+    /**
+     * 链路监控
+     *
+     * @return
+     */
+    @GetMapping("zipkin")
+    public String paymentZipkin() {
+        return "hi,i`am paymentzipkin server fall back.welcome to atguigu.hahahahahhahahah";
+    }
+
 }
